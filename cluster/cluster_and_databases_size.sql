21b7fca79c96586d2fc08cf1fff996c9060e3dce
Maiquel <103083422+maiquelgrassi@users.noreply.github.com>
2024-01-17 12:27:33 -0300

    Create cluster_and_databases_size.sql
    Total size of cluster and database sizes.

---
+++ cluster/cluster_and_databases_size.sql
@@ -0,0 +1,27 @@
+SELECT
+	'-' AS "RANKING"
+	,'CLUSTER (total)' AS "DATABASE" 
+	,pg_size_pretty(sum(pg_database_size(datname))) AS "SIZE"
+	,to_char(current_date,'DD/MM/YYYY') AS "QUERY DATE"
+	,to_char(localtime,'HH24:MI:SS')::text AS "QUERY TIME"
+FROM
+	pg_catalog.pg_database
+UNION ALL
+SELECT
+	 row_number() over()::text
+	,db
+	,pg_size_pretty(sz)
+	,dt
+	,hr
+FROM
+(
+	SELECT
+	 	 datname AS db
+		,pg_database_size(datname) AS sz
+		,to_char(current_date,'DD/MM/YYYY') AS dt
+		,to_char(localtime,'HH24:MI:SS')::text AS hr
+	FROM
+		 pg_catalog.pg_database
+	ORDER BY
+		 sz DESC
+) subquery;
