3117529bb69f0482f0a798863ab0571180bff2e8
Maiquel <103083422+maiquelgrassi@users.noreply.github.com>
2024-01-24 18:33:44 -0300

    Update cluster_and_database_sizes.sql

--- cluster/cluster_and_database_sizes.sql
+++ cluster/cluster_and_database_sizes.sql
@@ -2,7 +2,9 @@
 -- Tested on:
 	-- PostgreSQL 16.1, compiled by Visual C++ build 1937, 64-bit
 	-- PostgreSQL 16.1 (Debian 16.1-1.pgdg120+1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 12.2.0-14) 12.2.0, 64-bit
+-- Note: Theoretically, you can use it with all versions above 10. However, I haven't tested it with all of them.
 -- Created by: Maiquel O. Grassi
+
 SELECT
 	'-' AS "RANKING"
 	,'CLUSTER (total)' AS "DATABASE" 
