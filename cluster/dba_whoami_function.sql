cc107e29b27c625eaff8229280b7db8f77dcbe06
Maiquel <103083422+maiquelgrassi@users.noreply.github.com>
2024-09-23 16:43:46 -0300

    Update dba_whoami_function.sql

--- cluster/dba_whoami_function.sql
+++ cluster/dba_whoami_function.sql
@@ -1,11 +1,15 @@
--- Return:
--- Do you remember the Linux command 'whoami'? Well, it's like that,
--- this function returns very important information for the DBA.
--- Tested on:
--- PostgreSQL 16.1, compiled by Visual C++ build 1937, 64-bit
--- PostgreSQL 16.1 (Debian 16.1-1.pgdg120+1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 12.2.0-14) 12.2.0, 64-bit
--- Note: Theoretically, you can use it with all versions above 10. However, I haven't tested it with all of them.
--- Created by: Maiquel O. Grassi
+/*
+ * Return:
+ * Do you remember the Linux command 'whoami'? Well, it's like that,
+ * this function returns very important information for the DBA.
+
+ * Tested on:
+ * PostgreSQL 16.1, compiled by Visual C++ build 1937, 64-bit
+ * PostgreSQL 16.1 (Debian 16.1-1.pgdg120+1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 12.2.0-14) 12.2.0, 64-bit
+ * Note: Theoretically, you can use it with all versions above 10. However, I haven't tested it with all of them.
+
+ * Created by: Maiquel O. Grassi
+ */
 
 CREATE OR REPLACE FUNCTION public.dba_whoami()
 RETURNS TABLE ("Current Connection Information" text) AS
